fd30e02c29aa3637255166cba99ef4182a8443b9
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2023-12-06 14:26:09 +0100

    feat: add animation for burger icon

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -1,9 +1,9 @@
 document.querySelector('.burger-menu').addEventListener('click', function() {
-  if (this.classList.contains("burger-menu-clicked")) {
-    this.classList.remove("burger-menu-clicked");
-    this.classList.add("burger-menu-closing");
+  if (this.classList.contains("burger-menu--clicked")) {
+    this.classList.remove("burger-menu--clicked");
+    this.classList.add("burger-menu--closing");
   } else {
-    this.classList.add("burger-menu-clicked");
-    this.classList.remove("burger-menu-closing");
+    this.classList.add("burger-menu--clicked");
+    this.classList.remove("burger-menu--closing");
   }
 });

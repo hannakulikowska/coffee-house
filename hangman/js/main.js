9f10690624c5a91e2473579f4dcc2fa61374dbfa
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-01-13 16:37:00 +0100

    refactor: update code

--- hangman/js/main.js
+++ hangman/js/main.js
@@ -1,7 +1,2 @@
 import { createPage } from "./_create-page.js";
-import { createKeyboard } from "./_keyboard.js";
-
 document.addEventListener("DOMContentLoaded", createPage);
-
-import dataJSON from "../data/data.json" assert { type: "json" };
-console.log(dataJSON);
